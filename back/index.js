c1e535b8d397050294bd29f756748c2a44a03629
Pedrobear <plegulluche@gmail.com>
2022-11-22 16:46:31 +0100

    [ADD] adding bind mount for development and put the port on env variable for backend part

--- back/index.js
+++ back/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.status(200).send('hello world');
+    res.status(200).send('hello world !!!fsefsefsef ');
 });
 
 app.listen(config.port, config.host, (e)=> {
