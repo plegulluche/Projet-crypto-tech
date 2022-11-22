fd961acca8fee5527ba1b6bc850f59f361099fbc
Pedrobear <plegulluche@gmail.com>
2022-11-22 20:04:20 +0100

    [ADD] adding config file for docker for backend (node) for dev and prod environment

--- back/index.js
+++ back/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.status(200).send('hello world !!!fsefsefsef ');
+    res.status(200).send('hello world !');
 });
 
 app.listen(config.port, config.host, (e)=> {
