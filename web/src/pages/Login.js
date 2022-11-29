5524cda046331310f29f598927bd7fafd3fa8044
Ism005 <108396216+Ism005@users.noreply.github.com>
2022-11-29 12:45:42 +0100

    [ADD] Complete Login

--- web/src/pages/Login.js
+++ web/src/pages/Login.js
@@ -1,16 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import "./styles/Login.css";
+import GoogleButton from 'react-google-button'
 
-class Login extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() { 
-        return ( 
-            <div>
-                Login
+const Login = () => {
+
+    const testInput = () => {
+        let email = document.getElementById("email" ).value;
+        console.log(email);
+        let password = document.getElementById("password").value;
+        console.log(password);
+    };
+
+    return (
+        <div className="loginform">
+            <div className="top-form">
+                <h1 className="login-title">LOGIN</h1>
+            <div className="google-btn">
+            <GoogleButton
+                onClick={() => { console.log('Google button clicked') }}
+            />
             </div>
-         );
-    }
-}
- 
+           </div>
+            <div className="bottom-form">
+                <div className="email-input">
+                    <p className="email-text">Email adress :</p>
+                    <input className="descr" type="email" name="email" id="email"/>
+                </div>
+
+                <div className="password-input">
+                    <p className="password-text">Password :</p>
+                    <input className='descr' type="password" name="username" id="password"/>
+                </div>
+                <button className="test-button" type="button" name="button" id="test" onClick={() => testInput()}>Connect</button>
+            </div>
+        </div>
+    );
+};
+
 export default Login;
