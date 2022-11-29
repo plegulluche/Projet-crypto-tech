a041be278046cb8efa8163cec4c154c749a25723
Rafael Adragna <rafael.adragna@epitech.eu>
2022-11-29 09:02:39 -0600

    [FIX] now got an alert when register with 2 password non identical and adding responsive div for login and register

--- web/src/pages/Login.js
+++ web/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "./styles/Login.css";
 import GoogleButton from 'react-google-button'
+import Alert from 'react-bootstrap/Alert';
 
 class Login extends Component {
     constructor(props) {
@@ -9,34 +10,53 @@ class Login extends Component {
             ["LOGIN", "Email adress :", "Password :", "Connect"],
             ["CONNEXION", "Adresse mail :", "Mot de passe :", "Connexion"],
         ];
+        this.state = ({ show: false })
+        this.buttonClick = this.buttonClick.bind(this)
+        this.setShow = this.setShow.bind(this)
+    }
+
+    setShow(show){
+        this.setState({show})
     }
 
     buttonClick() {
-        let email = document.getElementById("email" ).value;
+        let email = document.getElementById("email").value;
         console.log(email);
         let password = document.getElementById("password").value;
         console.log(password);
+        // if () {                                      //verif si le login s'est bien passé
+        //     this.setState({ show: true });
+        // }
     };
 
-    render() { 
+    render() {
         return (
             <div className="loginform">
                 <div className="top-form">
                     <h1 className="login-title">{this.textLangage[this.props.language][0]}</h1>
-                <div className="google-btn">
-                <GoogleButton
-                    onClick={() => { console.log('Google button clicked') }}
-                />
+                    <div className="google-btn">
+                        <GoogleButton
+                            onClick={() => { console.log('Google button clicked') }}
+                        />
+                    </div>
                 </div>
-               </div>
+                {
+                this.state.show === true
+                    ?
+                    <Alert variant="danger" onClose={this.setShow} dismissible>
+                        <Alert.Heading>It seems your passwords are not identicals</Alert.Heading>
+                    </Alert>
+                    :
+                    <p></p>
+                }
                 <div className="bottom-form">
                     <div className="email-input">
                         <p className="email-text">{this.textLangage[this.props.language][1]}</p>
-                        <input className="descr" type="email" name="email" id="email"/>
+                        <input className="descr" type="email" name="email" id="email" />
                     </div>
                     <div className="password-input">
                         <p className="password-text">{this.textLangage[this.props.language][2]}</p>
-                        <input className='descr' type="password" name="username" id="password"/>
+                        <input className='descr' type="password" name="username" id="password" />
                     </div>
                     <button className="validate-button" type="button" name="button" id="test" onClick={this.buttonClick}>{this.textLangage[this.props.language][3]}</button>
                 </div>
@@ -44,5 +64,5 @@ class Login extends Component {
         );
     }
 }
- 
+
 export default Login;
--- web/src/pages/Register.js
+++ web/src/pages/Register.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import './styles/Register.css'
 import GoogleButton from 'react-google-button'
+import Alert from 'react-bootstrap/Alert';
 
 class Register extends Component {
     constructor() {
         super();
         this.textLangage = [
-            ["REGISTER", "Email adress :", "Password :", "RePassword :" ,"Create Account"],
+            ["REGISTER", "Email adress :", "Password :", "RePassword :", "Create Account"],
             ["S'INSCRIRE", "Adresse mail :", "Mot de passe :", "Re Mot de passe", "S'inscrire"],
         ];
-        this.state = { verif: 0 }
+        this.state = ({ show: false })
+        this.buttonClick = this.buttonClick.bind(this)
+        this.setShow = this.setShow.bind(this)
     }
 
-    buttonClick(){
+    setShow(show){
+        this.setState({show})
+    }
+
+    buttonClick() {
         let email = document.getElementById("email").value;
         console.log(email);
         let password = document.getElementById("password").value;
@@ -20,9 +27,7 @@ class Register extends Component {
         let repassword = document.getElementById("repassword").value;
         console.log(repassword);
         if (password !== repassword) {
-            this.setState({ verif: 1 });
-        } else {
-            this.setState({ verif: 0 });
+            this.setState({ show: true });
         }
     };
 
@@ -37,6 +42,15 @@ class Register extends Component {
                         />
                     </div>
                 </div>
+                {
+                this.state.show === true
+                    ?
+                    <Alert variant="danger" onClose={this.setShow} dismissible>
+                        <Alert.Heading>It seems your passwords are not identicals</Alert.Heading>
+                    </Alert>
+                    :
+                    <p></p>
+                }
                 <div className="bottom-form">
                     <div className="email-input">
                         <p className="email-text">{this.textLangage[this.props.language][1]}</p>
@@ -49,7 +63,7 @@ class Register extends Component {
                         <p className="password-text">{this.textLangage[this.props.language][3]}</p>
                         <input className='descr' type="password" name="username" id="repassword" />
                     </div>
-                    <button className="validate-button" type="button" name="button" id="test" onClick={this.testInput}>{this.textLangage[this.props.language][4]}</button>
+                    <button className="validate-button" type="button" name="button" id="test" onClick={this.buttonClick}>{this.textLangage[this.props.language][4]}</button>
                 </div>
             </div>
         );
