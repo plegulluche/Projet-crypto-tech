e7257650de181f816d754247d67d7aa4c8aa4122
Rafael Adragna <rafael.adragna@epitech.eu>
2022-12-06 07:46:20 -0600

    [FIX] now add more information about the crypto information widget [DEL] .idea in the root folder

--- web/src/components/crypto_widget/CryptoSummary.js
+++ web/src/components/crypto_widget/CryptoSummary.js
@@ -5,17 +5,44 @@ import logo from './bitcoin.png'
 class CryptoSummary extends Component {
     constructor(props) {
         super(props);
+        if (this.props.cryptoPercentage.charAt(0) === '-')
+            this.state = { check: false };
+        else
+            this.state = { check: true };
     }
-    render() { 
-        return ( 
+    render() {
+        return (
             <div className='cards'>
                 <div className='cHeader'>
-                    <img className='icon' src={logo} />
-                    {this.props.cryptoName}
+                    <img className='icon' alt='' src={logo} />
+                    <p className='cTitle'>{this.props.cryptoName}</p>
                 </div>
+                {this.props.isLogged === true ?
+                    <div>
+                        {this.state.check === true ?
+                            <div className='upCrypto'>
+                                <p>{this.props.cryptoPercentage}%</p><p className='cryptoDays'>/7d</p>
+                            </div>
+                            :
+                            <div className='downCrypto'>
+                                <p>{this.props.cryptoPercentage}%</p><p className='cryptoDays'>/7d</p>
+                            </div>
+                        }
+                        <div className='cryptoValue'>
+                            $16,309.92
+                        </div>
+                        <div className='lastDays'>
+                            Last 7 days
+                        </div>
+                    </div>
+                    :
+                    <div className='cryptoNotLog'>
+                        <p>Sign up to see<br/>more<br/>informations</p>
+                    </div>
+                }
             </div>
-         );
+        );
     }
 }
- 
+
 export default CryptoSummary;
