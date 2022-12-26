853f3d683aaaad3dbe72fb3cd4c98ad40b1b084a
plegulluche <plegulluche@gmail.com>
2022-12-26 13:50:46 +0100

    [ADD] adding user model in userModel.js, adding bcryptjs as dependecy

--- back/models/userModel.js
+++ back/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userShema = new mongoose.Schema({
   firstname: {
@@ -7,4 +8,47 @@ const userShema = new mongoose.Schema({
     required: [true, "Please state your firstname"],
     trim: true,
   },
+  lastname: {
+    type: String,
+    required: [true, "Please state your lastname"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please provide your email"],
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+    minLength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords are not the same !",
+    },
+  },
 });
+
+userShema.pre("save", async function (next) {
+  //Only run this function if password was actually modified
+  if (!this.isModified("password")) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete the passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model("User", userShema);
+
+module.exports = User;
