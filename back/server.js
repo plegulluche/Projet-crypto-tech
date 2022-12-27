b8ca654b79e4934f79166abee71f404ca8962169
Pierre-Legulluche <pierre.le-gulluche@epitech.eu>
2022-12-27 16:09:32 +0100

    [EDIT] changed the dotenv import order for the const app

--- back/server.js
+++ back/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const app = require("./app");
 
 dotenv.config({ path: "./config.env" });
+
+const app = require("./app");
 
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
