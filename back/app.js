86d43e12feb0af6f8de85a3dfe907902b0c2caff
Pierre-Legulluche <pierre.le-gulluche@epitech.eu>
2022-12-28 21:32:08 +0100

    [ADD] adding two error handling function in app.js , one for handling the unused routes and the other to handle global errors

--- back/app.js
+++ back/app.js
@@ -7,13 +7,10 @@ const app = express();
 
 // MIDDLEWARES
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
-
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 🤟');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -23,5 +20,24 @@ app.use((req, res, next) => {
 // ROUTES
 
 app.use('/api/v1/users', userRouter);
+
+//MIDDLE WARE TO HANDLE NON HANDLED ROUTES
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server !`,
+  });
+});
+
+// GLOBAL ERROR HANDLING MIDDLEWARE
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 500;
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
