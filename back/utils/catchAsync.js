5914889265ab07a7fffc966d68b7dddc4ba491e5
Pierre-Legulluche <pierre.le-gulluche@epitech.eu>
2022-12-30 19:20:00 +0100

    [ADD] adding more method for handling error in error controller , adding some comments and refactored catchAsync function for more readability, adding a command for lauching a prod env in package.json

--- back/utils/catchAsync.js
+++ back/utils/catchAsync.js
@@ -1,5 +1,6 @@
+// Function to catch asynchronous errors
 module.exports = (fn) => {
   return (req, res, next) => {
-    fn(req, res, next).catch(next);
+    fn(req, res, next).catch((err) => next(err));
   };
 };
