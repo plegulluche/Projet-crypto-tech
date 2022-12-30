9365ed9595d3a7766161b9e44fdd57764b086a37
Pierre-Legulluche <pierre.le-gulluche@epitech.eu>
2022-12-30 19:44:06 +0100

    [ADD] adding function to handle hunhandled errors in server.js to catch em globaly

--- back/server.js
+++ back/server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// HANDLE UNCAUGHTEXCEPTION ERROR AND SHUTTING DOWN APP GRACEFULLY
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION 🤯! Shutting down ....');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -17,6 +24,17 @@ mongoose.connect(DB).then(() => {
 
 // START SERVER
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`server running on port ${port} ...`);
+});
+
+// HANDLE UNHANDLEDREJECTION ERROR AND SHUTTING DOWN APP GRACEFULLY
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION 🤯! Shutting down ...');
+  // CLOSING SERVER
+  server.close(() => {
+    // CLOSING APP AFTER ALL PENDING REQUESTS ARE PROCESSED
+    process.exit(1);
+  });
 });
