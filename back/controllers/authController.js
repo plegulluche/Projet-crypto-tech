8711f4ba3b035265925b3aa723a53307b37bc9a0
Pierre-Legulluche <pierre.le-gulluche@epitech.eu>
2022-12-31 00:09:43 +0100

    [ADD] adding login function in authController, adding the corresponding route in userRoutes, created an instance method to check password in userModel

--- back/controllers/authController.js
+++ back/controllers/authController.js
@@ -1,6 +1,13 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const signToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
   console.log(req.body);
@@ -12,9 +19,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
@@ -22,5 +27,28 @@ exports.signup = catchAsync(async (req, res, next) => {
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1- Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password !', 400));
+  }
+
+  // 2- Check if user exists && password is correct
+  const user = await User.findOne({ email: email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+  // 3- If everything ok, send token to client
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'Success',
+    token,
   });
 });
--- back/models/userModel.js
+++ back/models/userModel.js
@@ -1,45 +1,46 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
-const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userShema = new mongoose.Schema({
   firstname: {
     type: String,
-    required: [true, "Please state your firstname"],
+    required: [true, 'Please state your firstname'],
     trim: true,
   },
   lastname: {
     type: String,
-    required: [true, "Please state your lastname"],
+    required: [true, 'Please state your lastname'],
     trim: true,
   },
   email: {
     type: String,
     unique: true,
-    required: [true, "Please provide your email"],
+    required: [true, 'Please provide your email'],
     lowercase: true,
-    validate: [validator.isEmail, "Please provide a valid email"],
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
   password: {
     type: String,
-    required: [true, "Please enter a password"],
+    required: [true, 'Please enter a password'],
     minLength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
-    required: [true, "Please confirm your password"],
+    required: [true, 'Please confirm your password'],
     validate: {
       validator: function (el) {
         return el === this.password;
       },
-      message: "Passwords are not the same !",
+      message: 'Passwords are not the same !',
     },
   },
 });
 
-userShema.pre("save", async function (next) {
+userShema.pre('save', async function (next) {
   //Only run this function if password was actually modified
-  if (!this.isModified("password")) return next();
+  if (!this.isModified('password')) return next();
 
   // Hash the password with cost of 12
   this.password = await bcrypt.hash(this.password, 12);
@@ -49,6 +50,15 @@ userShema.pre("save", async function (next) {
   next();
 });
 
-const User = mongoose.model("User", userShema);
+// INSTANCE METHOD (available on all documents of a collection)
+// CHECK IF A GIVEN PASSWORD IS THE SAME STORED IN THE DOCUMENT
+userShema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model('User', userShema);
 
 module.exports = User;
