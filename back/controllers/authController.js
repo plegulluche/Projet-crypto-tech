91f16b73ac2de62952202b80cd0a1b8fd7590c84
Pierre-Legulluche <pierre.le-gulluche@epitech.eu>
2023-01-01 21:18:12 +0100

    [ADD] adding promisify to authController, adding a protect method in authController to protect routes, aJsonwebtokenError and Tokenexpired error in errorController, adding protect method on getalluser in userRoutes.

--- back/controllers/authController.js
+++ back/controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
@@ -53,6 +54,33 @@ exports.login = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.protect = catchAsync((req, res, next) => {
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1 - Getting token and chack if it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError('Your are not logged in! Please log in to get access', 401)
+    );
+  }
+
+  // 2 - Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // 3 - Check if user still exists
+  const freshUser = await User.findById(decoded.id);
+  if (!freshUser) {
+    return next(
+      new AppError('The user belonging to the token no longer exists !', 401)
+    );
+  }
+  // 4 - Check if user changed password after the token was issued
+
   next();
 });
