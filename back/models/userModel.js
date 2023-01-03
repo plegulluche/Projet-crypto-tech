bf3fccfb41cc361f7d5552cada925530509517d5
Pierre-Legulluche <pierre.le-gulluche@epitech.eu>
2023-01-03 01:04:14 +0100

    [ADD] adding a route to change current password in userRoutes, adding a method to update password in authController

--- back/models/userModel.js
+++ back/models/userModel.js
@@ -36,6 +36,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please confirm your password'],
     validate: {
+      // This ONLY works for CREATE and SAVE !!
       validator: function (el) {
         return el === this.password;
       },
