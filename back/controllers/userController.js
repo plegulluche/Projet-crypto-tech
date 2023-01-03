0adae9c7bda3a4ed2b44115e7472acaac924b8a7
Pierre-Legulluche <pierre.le-gulluche@epitech.eu>
2023-01-03 12:04:37 +0100

    [ADD] adding a deleteMe protected route as a delete http method, adding deleteMe method in userController to set active field as false for a deleted user

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const { findByIdAndDelete, findByIdAndUpdate } = require('../models/userModel');
 
 const filterObj = (obj, ...allowedFileds) => {
   const newObj = {};
@@ -52,6 +53,15 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     data: {
       user: updatedUser,
     },
+  });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
 });
 
