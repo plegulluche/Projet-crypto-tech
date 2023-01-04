f3e010202d02cf8f45e40838ed6940a3d74cdbed
Ism005 <108396216+Ism005@users.noreply.github.com>
2023-01-04 17:01:08 +0100

    [ADD] add and modified Preferences.js, added logo for Preferences.js

--- web/src/pages/Preferences.js
+++ web/src/pages/Preferences.js
@@ -1,16 +1,55 @@
-import React, { Component } from 'react';
+import React, {Component, useState} from 'react';
+import Select from 'react-select';
+import './styles/Preferences.css';
+import logo1 from '../components/crypto_widget/bitcoin.png';
+import logo2 from '../components/crypto_widget/ltc.png';
+import logo3 from '../components/crypto_widget/eth.png';
+import logo4 from '../components/crypto_widget/bnb.png';
+import logo5 from '../components/crypto_widget/xrp.png';
+import logo6 from '../components/crypto_widget/dot.png';
+import logo7 from '../components/crypto_widget/ada.png';
+import logo8 from '../components/crypto_widget/shib.png';
+import logo9 from '../components/crypto_widget/sol.png';
+import logox from '../components/crypto_widget/matic.png';
 
-class Preferences extends Component {
-    constructor(props) {
-        super(props);
+
+const Preferences = () => {
+    const pref_title = ['Coin', 'Price'];
+    const [crypt, setCrypt] = useState([
+        {name: 'BTC', img: logo1, isFavorite: true },
+        {name: 'LTC', img: logo2, isFavorite: true },
+        {name: 'ETH', img: logo3, isFavorite: true },
+        {name: 'BNB', img: logo4, isFavorite: true },
+        {name: 'XRP', img: logo5, isFavorite: false },
+        {name: 'DOT', img: logo6, isFavorite: false },
+        {name: 'ADA', img: logo7, isFavorite: false },
+        {name: 'SHIB', img: logo8, isFavorite: false },
+        {name: 'SOL', img: logo9, isFavorite: false },
+        {name: 'MATIC', img: logox, isFavorite: false }
+    ]);
+    const submit = (e) => {
+
     }
-    render() { 
-        return ( 
-            <div>
-                Preferences
+
+    return (
+        <div>
+            <div className={'card-carousel'}>
+                        {crypt && crypt.map((card) => {
+                            return(
+                                    <div className={card.isFavorite?'card-crypt selected': 'card-crypt'}>
+                                                    <div className={'card-details'}>
+                                                        <img className={'crypto-pics'} src={card.img} alt={''}/>
+                                                        <p>{card.name}</p>
+                                                    </div>
+                                    </div>
+                            )
+                        })}
+                <input type={'button'} value={'submit'} className={'valid'} onClick={submit}>
+
+                </input>
+                </div>
             </div>
-         );
-    }
+    );
 }
- 
+
 export default Preferences;
